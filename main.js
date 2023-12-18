f1730d505f709dae9d72d122a39852e60172137d
“Abhinav <abhixk@amazon.com>
2023-12-18 16:31:20 +0530

    Added countdown finish workflow

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ let hoursItem = document.querySelector("#hours");
 let minItem = document.querySelector("#min");
 let secItem = document.querySelector("#sec");
 
+const body = document.body;
+const countdownDiv = document.querySelector(".countdown-container");
+const titleElement = document.querySelector(".title");
+
 let countDown = () => {
   let futureDate = new Date("20 Dec 2023 02:30");
   let currentDate = new Date();
@@ -12,6 +16,13 @@ let countDown = () => {
   let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
   let min = Math.floor(myDate / 1000 / 60) % 60;
   let sec = Math.floor(myDate / 1000) % 60;
+
+  if (days == 1 && hours == 9 && min == 55 && sec == 0) {
+    console.log(titleElement);
+    body.style.backgroundImage = 'url("images/pic2.jpeg")';
+    countdownDiv.style.display = "none";
+    titleElement.textContent = "AND THEY MET!!!";
+  }
 
   daysItem.innerHTML = days;
   hoursItem.innerHTML = hours;
